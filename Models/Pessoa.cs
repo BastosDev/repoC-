e44d30f587f5090b8f3eb8a1d0b94393cf9e241a
BastosDev <gustavoporto.gp63@gmail.com>
2025-05-06 18:11:22 -0300

    new Pessoa2

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,7 +9,6 @@ namespace repoC_.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
-
         public void Apresentar()
         {
             Console.WriteLine($"Meu nome é {Nome} e minha idade é {Idade}.");
--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@
 // repoC_.Models.Pessoa p = new repoC_.Models.Pessoa();
 using repoC_.Models;
 Pessoa p = new Pessoa();
+
+p.Nome = "Gustavo";
+p.Idade = 19;
+p.Apresentar();
